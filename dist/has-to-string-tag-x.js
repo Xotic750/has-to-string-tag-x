e8f19ce9c33edea4cd012a3f1fc5c2709d226498
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:29:06 +0200

    :bookmark: v2.0.17
    Add jsdelivr

--- dist/has-to-string-tag-x.js
+++ dist/has-to-string-tag-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-23T23:48:40.471Z",
+  "date": "2019-07-24T13:29:00.094Z",
   "describe": "",
   "description": "Tests if ES6 @@toStringTag is supported.",
   "file": "has-to-string-tag-x.js",
   "hash": "54be3c5a928b3ac29e78",
   "license": "MIT",
-  "version": "2.0.16"
+  "version": "2.0.17"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
