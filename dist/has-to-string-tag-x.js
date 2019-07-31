1e1d983dce15f282768ae4762abe952f08aa80fe
Graham Fairweather <xotic750@gmail.com>
2019-08-01 00:00:40 +0200

    :bookmark: v2.0.23
    Auto build

--- dist/has-to-string-tag-x.js
+++ dist/has-to-string-tag-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-31T19:29:08.692Z",
+  "date": "2019-07-31T22:00:34.793Z",
   "describe": "",
   "description": "Tests if ES6 @@toStringTag is supported.",
   "file": "has-to-string-tag-x.js",
   "hash": "54be3c5a928b3ac29e78",
   "license": "MIT",
-  "version": "2.0.22"
+  "version": "2.0.23"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
