8f1d33dfa2b24d2f8ee16d38860450236798428c
Graham Fairweather <xotic750@gmail.com>
2019-07-31 19:12:53 +0200

    Package rework

--- dist/has-to-string-tag-x.js
+++ dist/has-to-string-tag-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-31T15:43:05.968Z",
+  "date": "2019-07-31T17:12:48.137Z",
   "describe": "",
   "description": "Tests if ES6 @@toStringTag is supported.",
   "file": "has-to-string-tag-x.js",
